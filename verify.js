8f4f9209254d1625ffcc181eec516b7bcecfbbd8
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2020-01-14 10:09:16 +0000

    Added 'node verify' script to check for UUID Duplicates
    This script is intended to be called as a second GitHub Action after schema validation has been performed and has been passed.

---
+++ verify.js
@@ -0,0 +1,28 @@
+const db = require('.');
+
+const noDuplicates = Object.keys(db)
+  .filter(key => key !== 'schemas')
+  .map(list => {
+    let numberOfItems;
+    let numberOfUniques;
+    if (list == 'companies') {
+      let companyCodes = db[list].map(company => company.code);
+      numberOfItems = companyCodes.length;
+      numberOfUniques = (new Set(companyCodes)).size;  
+    } else {
+      // Service(s), Characteristic(s) and Descriptor(s)
+      let uuids = db[list].map(gatt => gatt.uuid);
+      numberOfItems = uuids.length;
+      numberOfUniques = (new Set(uuids)).size;
+    }
+
+    if (numberOfItems != numberOfUniques) {            
+      console.error(`Failed to verify '${list}':`);
+      console.error(`'${list}' contains '${numberOfItems - numberOfUniques}' duplicates.`);
+      return false;
+    }
+    return true;
+  })
+  .every(v => v); // true if all are true
+
+process.exit(noDuplicates ? 0 : 1);
