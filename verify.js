178afe63687b6bf659b2d112a0536f26f337a6ae
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2022-07-05 13:43:47 +0100

    Update verify.js
    Co-authored-by: Bence Frenyó <26139379+bencefr@users.noreply.github.com>

--- verify.js
+++ verify.js
@@ -28,25 +28,9 @@ const noDuplicates = Object.keys(db)
       numberOfItems = uuids.length;
 
       let identifiers = db[list].map(gatt => gatt.identifier);
-      let numberOfUniqueIdentifiers = (new Set(identifiers)).size;
-      if (numberOfItems !== numberOfUniqueIdentifiers) {
-        console.error(`Failed to verify '${list}': '${numberOfItems - numberOfUniqueIdentifiers}' duplicate Identifier(s) found.`);
-        
-        let identifierCount = {};
-        for(let i = 0; i < identifierCount.length; i++){ 
-          if (identifierCount[identifiers[i]]) {
-            identifierCount[identifiers[i]] += 1;
-          } else {
-            identifierCount[identifiers[i]] = 1;
-          } 
-        }
-
-        for (let identifier in identifierCount) {
-          if (identifierCount[identifier] >= 2) {
-            console.error(identifier + " found " + duplicateCounter[identifier] + " times.");
-          }
-        }
-
+      const duplicates = identifiers.filter((item, index) => identifiers.indexOf(item) !== index);
+      if (duplicates.length > 0) {
+        console.error(`Failed to verify '${list}', duplicate identifiers found: \n${duplicates.join('\n')}`);
         return false;
       }
 
